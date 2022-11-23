10fc6d5a09988f957e5c944cd145ca121cd6235b
Jacky Ngo <jacky.ngo96@gmail.com>
2022-11-23 17:01:26 -0500

    Added a header Partial

--- express_server.js
+++ express_server.js
@@ -23,4 +23,16 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+}); 
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+}); 
+
+app.get("/urls/:id", (req, res) => {
+  const shortUrl = req.params.id
+  const longURL = urlDatabase[shortUrl]
+  const templateVars = { id: shortUrl, longURL};
+  res.render("urls_show", templateVars);
 });
